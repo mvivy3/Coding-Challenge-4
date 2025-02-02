3971b813eb9b0f39006fe315cff89ca76ff3d973
mvivy3 <meredithyen@usf.edu>
2025-02-02 14:12:08 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,11 @@
+// Task 1: Customer Discounts
+let purchaseAmount = 5000; // Declare purchaseAmount with a value
+
+if (purchaseAmount > 100 ) { // Check to see if the purchaseAmountis greater than $100
+    } else {
+    }
+    function calculateDiscount() { // Define a function that calculates tax
+        return this.purchaseAmount * 0.10; // Multiply the amount by 10% tax rate and return the value
+    }
+    finalAmount = purchaseAmount * 0.10;
+    console.log(`Final amount after discount: $${finalAmount}`);
