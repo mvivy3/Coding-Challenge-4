81a2034c088e080f1d7182562cea6cd1cfdae1ed
mvivy3 <meredithyen@usf.edu>
2025-02-02 17:32:58 -0500

    Task 5 - Employee Information

--- cc_4.js
+++ cc_4.js
@@ -27,7 +27,7 @@ while (stock >= 0) { // Use a while loop to decrease stock until it reaches zero
     stock--; // Decrement the value of count by 1
 
 
-    // Task 4: Customer Survey
+// Task 4: Customer Survey
 let responses = 0; // Declare responses with an initial value of 0
 
 do { // Use a do...while loop to collect responses until it reaches 3
@@ -35,3 +35,14 @@ do { // Use a do...while loop to collect responses until it reaches 3
     responses++ // simulating user input 
 } while (responses <= 3);
 console.log(responses); // Log each response count
+
+
+// Task 5: Employee Information
+let employee = {
+    name: "Teddy",
+    position: "Manager",
+    salary: 100000
+};
+for (let property in employee) { // Use a for...in loop
+    console.log(`${property}: ${employee[property]}`); // Log each property
+}
